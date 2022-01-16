84236c00f726649643b872b00aff0e30aeb475f1
Bhavith-123 <97665580+Bhavith-123@users.noreply.github.com>
2022-01-17 00:23:15 +0530

    Add files via upload

---
+++ www/js/scrip.js
@@ -0,0 +1,8 @@
+ <script type="text/javascript"><!--
+    // These variables are used by the web2py_ajax_init function in web2py_ajax.js (which is loaded below).
+    var w2p_ajax_confirm_message = "Are you sure you want to delete this object?";
+    var w2p_ajax_disable_with_message = "Working...";
+    var w2p_ajax_date_format = "%Y-%m-%d";
+    var w2p_ajax_datetime_format = "%Y-%m-%d %H:%M:%S";
+    var ajax_error_500 = 'An error occured, please <a href="/adm/ug">reload</a> the page'
+    //--></script>
